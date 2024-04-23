eb006e80c04308904c8e9f8150b9cad30f512045
droc101 <37421449+droc101@users.noreply.github.com>
2024-04-23 12:03:02 -0500

    fix warning

--- error.c
+++ error.c
@@ -9,7 +9,7 @@
 #include "Helpers/font.h"
 #include "Structs/Vector2.h"
 
-_Noreturn void Error(const char* error) {
+_Noreturn void Error(char* error) {
     FontDrawString(vec2(20, 20), error, 32);
     SDL_RenderPresent(GetRenderer());
     SDL_Delay(5000);
--- error.h
+++ error.h
@@ -6,6 +6,6 @@
 #define GAME_ERROR_H
 
 // Print an error message on screen and lock up
-_Noreturn void Error(const char* error);
+_Noreturn void Error(char* error);
 
 #endif //GAME_ERROR_H
