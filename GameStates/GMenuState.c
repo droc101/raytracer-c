9f30c9548f98bd25423fb1972c63a058baa0724c
droc101 <37421449+droc101@users.noreply.github.com>
2024-04-24 21:18:13 -0500

    Update GMenuState.c

--- GameStates/GMenuState.c
+++ GameStates/GMenuState.c
@@ -2,6 +2,7 @@
 // Created by droc101 on 4/22/2024.
 //
 
+#include <stdio.h>
 #include "GMenuState.h"
 #include "../input.h"
 #include "../Structs/ray.h"
@@ -12,16 +13,38 @@
 
 #include "../Structs/ray.h"
 
+Wall *tWall;
+Vector2 tPlayerPos;
+double tPlayerRot;
+
 void GMenuStateUpdate() {
+
+    // get the current mouse position
+    int x, y;
+    SDL_GetMouseState(&x, &y);
+
+    // make the player look at the mouse
+    tPlayerRot = atan2(y - tPlayerPos.y, x - tPlayerPos.x);
+
+    if (IsKeyPressed(SDL_SCANCODE_W)) {
+        tPlayerPos.y -= 1;
+    } else if (IsKeyPressed(SDL_SCANCODE_S)) {
+        tPlayerPos.y += 1;
+    }
+
+    if (IsKeyPressed(SDL_SCANCODE_A)) {
+        tPlayerPos.x -= 1;
+    } else if (IsKeyPressed(SDL_SCANCODE_D)) {
+        tPlayerPos.x += 1;
+    }
+
     if (IsKeyJustPressed(SDL_SCANCODE_SPACE)) {
         // change to the main game state
         GMainStateSet();
     }
 }
 
-Wall *tWall;
-Vector2 tPlayerPos;
-double tPlayerRot;
+
 
 void GMenuStateRender() {
     setColorUint(0xFF123456);
@@ -29,6 +52,8 @@ void GMenuStateRender() {
 
     //FontDrawString(vec2(20, 20), "GAME.", 128);
     //FontDrawString(vec2(20, 150), "Press Space to start.", 32);
+
+    FontDrawString(vec2(10, 10), "wasd to move test player, mouse to look, space to start", 16);
 
     setColorUint(0xFFFFFFFF);
     SDL_RenderDrawLine(GetRenderer(), tWall->a.x, tWall->a.y, tWall->b.x, tWall->b.y);
@@ -47,15 +72,22 @@ void GMenuStateRender() {
 
         char buffer[256];
         sprintf(buffer, "Collision Point: %f, %f\nPushed Point: %f, %f", rc.CollisonPoint.x, rc.CollisonPoint.y, PushedPos.x, PushedPos.y);
-        FontDrawString(vec2(20, 200), buffer, 32);
+        FontDrawString(vec2(20, 200), buffer, 16);
+
+        setColorUint(0xFF0000FF);
+        SDL_RenderDrawLine(GetRenderer(), tPlayerPos.x, tPlayerPos.y, rc.CollisonPoint.x, rc.CollisonPoint.y);
+    } else {
+        FontDrawString(vec2(20, 200), "No Collision", 16);
+        Vector2 dir = vec2(cos(tPlayerRot), sin(tPlayerRot));
+        dir = Vector2Scale(dir, 1024);
+        dir = Vector2Add(tPlayerPos, dir);
+        setColorUint(0xFF0000FF);
+        SDL_RenderDrawLine(GetRenderer(), tPlayerPos.x, tPlayerPos.y, dir.x, dir.y);
     }
 
-    Vector2 dir = vec2(cos(tPlayerRot), sin(tPlayerRot));
-    dir = Vector2Scale(dir, 20);
-    dir = Vector2Add(tPlayerPos, dir);
 
-    setColorUint(0xFF0000FF);
-    SDL_RenderDrawLine(GetRenderer(), tPlayerPos.x, tPlayerPos.y, dir.x, dir.y);
+
+
 
 }
 
