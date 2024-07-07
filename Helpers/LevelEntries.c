0a686d6cb039be54d70eebeb907c195aacf32930
droc101 <37421449+droc101@users.noreply.github.com>
2024-07-06 22:16:39 -0500

    add big level (id 3 "indoor")
    Signed-off-by: droc101 <37421449+droc101@users.noreply.github.com>

--- Helpers/LevelEntries.c
+++ Helpers/LevelEntries.c
@@ -7,7 +7,7 @@
 LevelEntry gLevelEntries[LEVEL_COUNT] = {
         DEFINE_LEVEL("stage1", test_level, "Test Level", true),
         DEFINE_LEVEL("stage2", hallway, "Hallway", true),
-        STUB_LEVEL(),
+        DEFINE_LEVEL("indoor", hub, "Indoor Test", false),
         STUB_LEVEL(),
         STUB_LEVEL(),
         STUB_LEVEL(),
