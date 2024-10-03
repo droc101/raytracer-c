6616e73210f7d1b1567eb3b7f05ae4bedbf3687e
NBT22 <noah.thwing@gmail.com>
2024-10-03 16:41:49 -0500

    Fix ActorTransformMatrix

--- Helpers/RenderingHelpers.c
+++ Helpers/RenderingHelpers.c
@@ -37,12 +37,11 @@ mat4 *GetMatrix(Camera *cam) {
     return MODEL_VIEW_PROJECTION;
 }
 
-// TODO: rotation is not working
 mat4 *ActorTransformMatrix(Actor *Actor) {
     mat4 *MODEL = malloc(sizeof(mat4));
     glm_mat4_identity(*MODEL);
-    glm_rotate(*MODEL, glm_rad(Actor->rotation), (vec3){0, 1, 0});
     glm_translate(*MODEL, (vec3){Actor->position.x, 0, Actor->position.y});
+    glm_rotate(*MODEL, -Actor->rotation, (vec3){0, 1, 0});
     return MODEL;
 }
 
