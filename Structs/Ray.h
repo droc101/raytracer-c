0db4dd48b810bf7b8edb7c769d6e8ea353f90de8
droc101 <37421449+droc101@users.noreply.github.com>
2024-10-08 21:11:34 -0500

    Remove `HitscanLevel` as it is unused since gl
    Signed-off-by: droc101 <37421449+droc101@users.noreply.github.com>

--- Structs/Ray.h
+++ Structs/Ray.h
@@ -18,16 +18,4 @@
  */
 RayCastResult Intersect(Wall wall, Vector2 from, double direction);
 
-/**
- * Perform a raycast against a level
- * @param l Level to check
- * @param pos Ray origin
- * @param angle Ray angle
- * @param scanWalls Whether to scan walls
- * @param scanActors Whether to scan actors
- * @param alwaysCollideActors Whether to always collide actors
- * @return @c RayCastResult with hit information
- */
-RayCastResult HitscanLevel(Level l, Vector2 pos, double angle, bool scanWalls, bool scanActors, bool alwaysCollideActors);
-
 #endif //GAME_RAY_H
