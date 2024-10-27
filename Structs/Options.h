ac6b9d0432b7fe92c9728464c6a55edc73954d76
droc101 <37421449+droc101@users.noreply.github.com>
2024-10-27 15:08:39 -0500

    document Options.h

--- Structs/Options.h
+++ Structs/Options.h
@@ -7,8 +7,16 @@
 
 #include "../defines.h"
 
+/**
+ * Read options from disk, or load defaults if no options file is found or is invalid
+ * @param options The options to load into
+ */
 void LoadOptions(Options *options);
 
+/**
+ * Save options to disk
+ * @param options The options to save
+ */
 void SaveOptions(Options *options);
 
 #endif //GAME_OPTIONS_H
