b7581aa6a8be33c3e93b3a9f112c1c3a1ef99991
NBT22 <noah.thwing@gmail.com>
2024-11-18 22:19:10 -0600

    Logging Changes Improvements

--- Helpers/Core/Logging.c
+++ Helpers/Core/Logging.c
@@ -3,30 +3,26 @@
 //
 
 #include "Logging.h"
-#include <stdarg.h>
 #include <stdio.h>
-#include "../../defines.h"
 
 /// The length of the longest value passed to the type argument of the LogInternal function, plus one
 #define longestType 6
 
-void LogInternal(const char *type, const int color, const char *message, const va_list args)
+void LogInternal(const char *type, const int color, const bool flush, const char *message, const va_list args)
 {
     char buf[9 + longestType];
     sprintf(buf, "\x1b[%02d;49m[%s]", color, type);
     printf("%-1"TO_STR(longestType)"s", buf);
     vprintf(message, args);
     printf("\x1b[0m");
-#ifdef FLUSH_ON_ERROR
-    fflush(stdout);
-#endif
+    if (flush) fflush(stdout);
 }
 
 void LogInfo(const char *message, ...)
 {
     va_list args;
     va_start(args, message);
-    LogInternal("INFO", 37, message, args);
+    LogInternal("INFO", 37, FLUSH_ON_INFO, message, args);
     va_end(args);
 }
 
@@ -35,7 +31,7 @@ void LogDebug(const char *message, ...)
 #ifndef NDEBUG
     va_list args;
     va_start(args, message);
-    LogInternal("DEBUG", 37, message, args);
+    LogInternal("DEBUG", 37, FLUSH_ON_DEBUG, message, args);
     va_end(args);
 #endif
 }
@@ -44,7 +40,7 @@ void LogWarning(const char *message, ...)
 {
     va_list args;
     va_start(args, message);
-    LogInternal("INFO", 33, message, args);
+    LogInternal("INFO", 33, FLUSH_ON_WARNING, message, args);
     va_end(args);
 }
 
@@ -52,6 +48,6 @@ void LogError(const char *message, ...)
 {
     va_list args;
     va_start(args, message);
-    LogInternal("ERROR", 31, message, args);
+    LogInternal("ERROR", 31, FLUSH_ON_ERROR, message, args);
     va_end(args);
 }
