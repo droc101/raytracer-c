a6257b4f16090e41f82048503e005f2d0d587a08
NBT22 <noah.thwing@gmail.com>
2024-11-19 14:01:55 -0600

    Logging Changes Finale Finale

--- Helpers/Core/Logging.h
+++ Helpers/Core/Logging.h
@@ -17,7 +17,6 @@ void LogInternal(const char *type, int color, bool flush, const char *message, .
 
 /**
  * Log an info message
- * @param message Format string
  * @param ... Format arguments
  */
 #define LogInfo(...) LogInternal("INFO", 37, FLUSH_ON_INFO, __VA_ARGS__)
@@ -25,7 +24,6 @@ void LogInternal(const char *type, int color, bool flush, const char *message, .
 #ifndef NDEBUG
 /**
  * Log an info message, but only in debug builds
- * @param message Format string
  * @param ... Format arguments
  */
 #define LogDebug(...) LogInternal("DEBUG", 37, FLUSH_ON_DEBUG, __VA_ARGS__)
@@ -33,14 +31,12 @@ void LogInternal(const char *type, int color, bool flush, const char *message, .
 
 /**
  * Log a warning message
- * @param message Format string
  * @param ... Format arguments
  */
 #define LogWarning(...) LogInternal("INFO", 33, FLUSH_ON_WARNING, __VA_ARGS__)
 
 /**
  * Log an error message
- * @param message Format string
  * @param ... Format arguments
  */
 #define LogError(...) LogInternal("ERROR", 31, FLUSH_ON_ERROR, __VA_ARGS__)
