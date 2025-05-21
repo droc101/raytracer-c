9fe54df214f5e9001ddacb3598622b86586fcedd
droc101 <37421449+droc101@users.noreply.github.com>
2025-05-21 12:45:49 -0500

    so apparently it's zlib1.dll not libzlib1.dll now

--- postbuild-windows.py
+++ postbuild-windows.py
@@ -13,4 +13,4 @@ mingw_path = os.popen("cygpath -w /mingw64").read().strip()
 
 shutil.copy("lib/SDL2/x86_64-w64-mingw32/bin/SDL2.dll", build_dir + "/SDL2.dll")
 shutil.copy("lib/SDL2_mixer/x86_64-w64-mingw32/bin/SDL2_mixer.dll", build_dir + "/SDL2_mixer.dll")
-shutil.copy(mingw_path + "/bin/libzlib1.dll", build_dir + "/libzlib1.dll")
+shutil.copy(mingw_path + "\\bin\\zlib1.dll", build_dir + "/zlib1.dll")
