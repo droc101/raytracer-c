65c339ce658e58669e83356fa90dcef479f9f97f
droc101 <37421449+droc101@users.noreply.github.com>
2025-05-21 12:42:33 -0500

    try to get zlib dll from msys2

--- postbuild-windows.py
+++ postbuild-windows.py
@@ -2,13 +2,15 @@
 
 import shutil
 import sys
+import os
 
 if len(sys.argv) != 2:
 	print("Usage: postbuild-windows.py <path-to-build-dir>")
 	sys.exit(1)
 
 build_dir = sys.argv[1]
+mingw_path = os.popen("cygpath -w /mingw64").read().strip()
 
 shutil.copy("lib/SDL2/x86_64-w64-mingw32/bin/SDL2.dll", build_dir + "/SDL2.dll")
 shutil.copy("lib/SDL2_mixer/x86_64-w64-mingw32/bin/SDL2_mixer.dll", build_dir + "/SDL2_mixer.dll")
-# shutil.copy("lib/zlib/bin/libzlib1.dll", build_dir + "/libzlib1.dll")
+shutil.copy(mingw_path + "/bin/libzlib1.dll", build_dir + "/libzlib1.dll")
